894573ebdda8e06d77bea8045c24e45cc727aee2
Chris Williams <christophw+github@protonmail.com>
2020-02-14 11:58:49 +0000

    scaffold

---
+++ theme/gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = options => {
+  return {
+    siteMetadata: {
+      title: `Gatsby Theme Cactus`,
+      description: `Gatsby Theme Cactus`,
+      siteUrl: `https://github.com/chrismwilliams/gatsby-theme-a11y-controls`,
+      author: `Chris Williams`,
+      lang: `en`,
+    },
+    plugins: [],
+  };
+};
